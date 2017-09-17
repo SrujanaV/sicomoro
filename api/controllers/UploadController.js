1aa0217495621df96a004564d7ee3d2b180e4951
Chintan <chintan@wohlig.com>
2017-09-17 15:45:15 +0530

    google cloud storage is in place

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side logic for managing uploads
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+global.storageBasket = "wohlig";
 module.exports = {
     index: function (req, res) {
         var fileNames = [];
@@ -12,7 +12,7 @@ module.exports = {
             adapter: require('skipper-gclouds'),
             projectId: 'wohligerp',
             keyFilename: './config/storage/googleCloudStorageKey.json',
-            bucket: 'wohlig',
+            bucket: storageBasket,
             public: true,
         }, function (err, data) {
             res.callback(err, _.map(data, function (n) {
@@ -21,31 +21,33 @@ module.exports = {
         });
     },
     readFile: function (req, res) {
-        if (req.query.file) {
-            var width;
-            var height;
-            if (req.query.width) {
-                width = parseInt(req.query.width);
-                if (_.isNaN(width)) {
-                    width = undefined;
-                }
-            }
-            if (req.query.height) {
-                height = parseInt(req.query.height);
-                if (_.isNaN(height)) {
-                    height = undefined;
-                }
-            }
-            Config.readUploaded(req.query.file, width, height, req.query.style, res);
-        } else {
-            res.callback("No Such File Found");
-        }
-
+        // if (req.query.file) {
+        //     var width;
+        //     var height;
+        //     if (req.query.width) {
+        //         width = parseInt(req.query.width);
+        //         if (_.isNaN(width)) {
+        //             width = undefined;
+        //         }
+        //     }
+        //     if (req.query.height) {
+        //         height = parseInt(req.query.height);
+        //         if (_.isNaN(height)) {
+        //             height = undefined;
+        //         }
+        //     }
+        //     Config.readUploaded(req.query.file, width, height, req.query.style, res);
+        // } else {
+        //     res.callback("No Such File Found");
+        // }
+        console.log("https://storage.googleapis.com/" + storageBasket + "/" + req.query.file);
+        return res.redirect("https://storage.googleapis.com/" + storageBasket + "/" + req.query.file);
     },
     // readAttachment: function(req, res){
     //     Config.readAttachment
     // },
     wallpaper: function (req, res) {
-        Config.readUploaded(req.query.file, req.query.width, req.query.height, req.query.style, res);
+        // Config.readUploaded(req.query.file, req.query.width, req.query.height, req.query.style, res);
+        return res.redirect("https://storage.googleapis.com/" + storageBasket + "/" + req.query.file);
     }
 };
