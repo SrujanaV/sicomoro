c1025badfcf5f9422512c1ed3be02ea1ea7fa876
Chintan <chintan@wohlig.com>
2017-09-26 01:00:50 +0530

    ok

--- temp.js
+++ temp.js
@@ -1,22 +1,11 @@
-// var JSHINT = require("jshint").JSHINT;
-// var jetpack = require('fs-jetpack');
-// var _ = require("lodash");
+var JSHINT = require("jshint").JSHINT;
+var jetpack = require('fs-jetpack');
+var _ = require("lodash");
 
-// var source = jetpack.read('api/services/User.js');
-// JSHINT(source, {
-//     undef: false,
-//     maxdepth: 1
-// });
-// var report = JSHINT.data();
-// jetpack.write("demo.json", report);
-
-
-
-
-function abc() {
-    console.log(arguments);
-}
-
-abc.apply(null, [1, 2, 3, 4, 5, "Chintan", function () {
-    console.log(123);
-}]);
+var source = jetpack.read('api/services/User.js');
+JSHINT(source, {
+    undef: false,
+    maxdepth: 1
+});
+var report = JSHINT.data();
+jetpack.write("demo.json", report);
